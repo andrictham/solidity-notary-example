27c2ce5a8ea30acbdb6009c1230c053794181f9b
Andric Tham <andric@nuisance.co>
2017-07-28 13:51:56 +0800

    ProofOfExistence2: Notarize multiple documents

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var ProofOfExistence1 = artifacts.require("ProofOfExistence1");
+var ProofOfExistence2 = artifacts.require("ProofOfExistence2");
 
 module.exports = function(deployer) {
   deployer.deploy(ProofOfExistence1);
+  deployer.deploy(ProofOfExistence2);
 };
