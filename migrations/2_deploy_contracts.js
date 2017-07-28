e55ad6eec5a26d83c92b3bffede0a894c2937d57
Andric Tham <andric@nuisance.co>
2017-07-28 14:02:00 +0800

    ProofOfExistence3: Convert state variable from array to mapping, get rid of “for” loop

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
-var ProofOfExistence1 = artifacts.require("ProofOfExistence1");
-var ProofOfExistence2 = artifacts.require("ProofOfExistence2");
+// var ProofOfExistence1 = artifacts.require("ProofOfExistence1");
+// var ProofOfExistence2 = artifacts.require("ProofOfExistence2");
+var ProofOfExistence3 = artifacts.require("ProofOfExistence3");
 
 module.exports = function(deployer) {
-  deployer.deploy(ProofOfExistence1);
-  deployer.deploy(ProofOfExistence2);
+  // deployer.deploy(ProofOfExistence1);
+  // deployer.deploy(ProofOfExistence2);
+  deployer.deploy(ProofOfExistence3);
 };
